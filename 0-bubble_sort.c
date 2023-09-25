5dcab6dfda4f463bf2cf02830786890d82245853
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-09-25 05:57:11 +0000

    Using Burble sort to sort integers in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@ void bubble_sort(int *array, size_t size)
 	int swapped;
 	size_t i, j, temp, k;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 
-		for (j = 0; j < size - 1 - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
