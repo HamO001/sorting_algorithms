6fa35d77cde6ec048f04c4b8eb4968ab44a42bef
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-09-25 05:44:40 +0000

    Using Burble sort to sort integers in ascending order

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int swapped;
+	size_t i, j, temp, k;
+
+	for (i = 0; i < size; i++)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - 1 - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/*wap elements*/
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+
+				/*print array after swap*/
+				for (k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+
+	/*if no swap occured in this pass, the array is already sorted*/
+		if (swapped == 0)
+			break;
+	}
+
+}
