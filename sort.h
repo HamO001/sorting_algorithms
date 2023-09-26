3caaf7590f07acfcadbfd6631872db4d3103e12d
HamO001 <ndarugastiv9@gmail.com>
2023-09-26 10:18:39 -0700

    100strong

--- sort.h
+++ sort.h
@@ -14,5 +14,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *prev, listint_t *temp);
 void selection_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
